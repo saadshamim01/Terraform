3bcac0c719b048b758f7cf0e4ab6d5779a600077
Saad Shamim <saadshamim01@gmail.com>
2021-09-17 03:36:31 +0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -14,158 +14,53 @@ provider "aws" {
   secret_key = var.secret_key
 }
 
-# 1. Create VPC
-
-resource "aws_vpc" "prod-vpc" {
-  cidr_block = "10.0.0.0/16"
-  tags = {
-    Name = "production"
-  }
-}
-
-# 2. Create internet gateway
-
-resource "aws_internet_gateway" "gw" {
-  vpc_id = aws_vpc.prod-vpc.id
-
-}
-
-# 3. Create Custom Route Table
-
-resource "aws_route_table" "prod-route-table" {
-  vpc_id = aws_vpc.prod-vpc.id
-
-  route {
-      # Routing all the traffic to gateway
-      cidr_block = "0.0.0.0/0"
-      gateway_id = aws_internet_gateway.gw.id
-    }
-  route {
-      ipv6_cidr_block        = "::/0"
-      gateway_id = aws_internet_gateway.gw.id
-    }
-  tags = {
-    Name = "Prod"
-  }
-}
-
-# 4. Create a subnet
-
-resource "aws_subnet" "subnet-1" {
-  vpc_id     = aws_vpc.prod-vpc.id
-  cidr_block = "10.0.1.0/24"
-  availability_zone = "ap-south-1a"
-  tags = {
-    Name = "prod-subnet"
-  }
-}
-
-# 5. Associate subnet with Route Table
-
-resource "aws_route_table_association" "a" {
-  subnet_id      = aws_subnet.subnet-1.id
-  route_table_id = aws_route_table.prod-route-table.id
-}
-
-# 6. Create security group to allow port 22, 80, 443
-
-resource "aws_security_group" "allow_web" {
-  name        = "allow_web_traffic"
-  description = "Allow web traffic"
-  vpc_id      = aws_vpc.prod-vpc.id
+#Create security group with firewall rules
+resource "aws_security_group" "my_security_group" {
+  name        = var.security_group
+  description = "security group for Ec2 instance"
 
   ingress {
-      description      = "HTTPS"
-      from_port        = 443
-      to_port          = 443
-      protocol         = "tcp"
-      cidr_blocks      = ["0.0.0.0/0"]
-      ipv6_cidr_blocks = ["0.0.0.0/0"]
-    }
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
-  ingress {
-      description      = "HTTP"
-      from_port        = 80
-      to_port          = 80
-      protocol         = "tcp"
-      cidr_blocks      = ["0.0.0.0/0"]
-      ipv6_cidr_blocks = ["0.0.0.0/0"]
-    }
-  ingress {
-      description      = "SSH"
-      from_port        = 22
-      to_port          = 22
-      protocol         = "tcp"
-      cidr_blocks      = ["0.0.0.0/0"]
-      ipv6_cidr_blocks = ["0.0.0.0/0"]
-    }
+ ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
-
+ # outbound from jenkis server
   egress {
-      from_port        = 0
-      to_port          = 0
-      protocol         = "-1"
-      cidr_blocks      = ["0.0.0.0/0"]
-      ipv6_cidr_blocks = ["::/0"]
-    }
+    from_port   = 0
+    to_port     = 65535
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
-  tags = {
-    Name = "allow_web"
+  tags= {
+    Name = var.security_group
   }
 }
 
-# 7. Create a network interface with an ip in the subnet that was created in step 4.
-resource "aws_network_interface" "web-server-nic" {
-  subnet_id       = aws_subnet.subnet-1.id
-  private_ips     = ["10.0.1.50"]
-  security_groups = [aws_security_group.allow_web.id]
-
-}
-
-output "server_public_ip" {
-  value = aws_eip.one.public_ip
-}
-
-# 8. Assign an elastic IP to the netork interface
-
-resource "aws_eip" "one" {
-  vpc                       = true
-  network_interface         = aws_network_interface.web-server-nic.id
-  associate_with_private_ip = "10.0.1.50"
-  depends_on = [aws_internet_gateway.gw]
-}
-
-# 9. Create Ubuntu server and install/enable apache2
-
-resource "aws_instance" "my-web-server" {
-  ami           = "ami-0c1a7f89451184c8b"
-  instance_type = "t2.micro"
-  availability_zone = "ap-south-1a"
-  key_name = "terraform-access-key"
-
-  network_interface {
-    device_index = 0
-    network_interface_id = aws_network_interface.web-server-nic.id
-  }
-
-  user_data = <<-EOF
-                #!/bin/bash
-                sudo apt update -y
-                sudo apt install apache2 -y
-                sudo systemctl start apache2
-                sudo bash -c 'echo your very first web server > /var/www/html/index.html'
-                EOF
-
-  tags = {
-    Name = "web-server"
+# Create AWS ec2 instance
+resource "aws_instance" "myFirstInstance" {
+  ami           = var.ami_id
+  key_name = var.key_name
+  instance_type = var.instance_type
+  security_groups= [var.security_group]
+  tags= {
+    Name = var.tag_name
   }
 }
 
-output "server_private_ip"{
-  value = aws_instance.my-web-server.private_ip
-}
-
-output "server_id"{
-  value = aws_instance.my-web-server.id
-}
-
+# Create Elastic IP address
+resource "aws_eip" "myFirstInstance" {
+  vpc      = true
+  instance = aws_instance.myFirstInstance.id
+tags= {
+    Name = "my_elastic_ip"
+  }
