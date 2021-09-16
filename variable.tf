acd5110c78087fd323418aa1ee159d886203a98d
Saad Shamim <saadshamim01@gmail.com>
2021-09-17 03:37:08 +0400

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -6,3 +6,28 @@ variable "access_key"{
 variable "secret_key"{
 
 }
+
+variable "key_name" {
+  description = " SSH keys to connect to ec2 instance"
+  default     =  "myJune2021Key"
+}
+
+variable "instance_type" {
+  description = "instance type for ec2"
+  default     =  "t2.micro"
+}
+
+variable "security_group" {
+  description = "Name of security group"
+  default     = "my-jenkins-security-group"
+}
+
+variable "tag_name" {
+  description = "Tag Name of for Ec2 instance"
+  default     = "my-ec2-instance"
+}
+
+variable "ami_id" {
+  description = "AMI for Ubuntu Ec2 instance"
+  default     = "ami-0b9064170e32bde34"
+}
